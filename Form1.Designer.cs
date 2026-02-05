441784b8c8bf09a97330b987916d1938ea8efbbd
SAMSUNG <SAMSUNG@LAPTOP-9FD33DQF>
2026-02-05 09:18:25 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace archivos_de_texto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(679, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(712, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(6, 45);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(788, 402);
+            this.webView21.Source = new System.Uri("https://www.google.com", System.UriKind.Absolute);
+            this.webView21.TabIndex = 2;
+            this.webView21.ZoomFactor = 1D;
+            this.webView21.Click += new System.EventHandler(this.webView21_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+
+
+namespace archivos_de_texto
+{
+    public partial class Form1 : Form
+    {
+        string archivoHistorial = "historial.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void InicializarWebView()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+
+        // Función para guardar en el archivo
+        private void Guardar(string nombreArchivo, string texto)
+        {
+            FileStream stream = new FileStream(nombreArchivo, FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine(texto);
+            writer.Close();
+        }
+        
+        // Botón navegar
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string url = comboBox1.Text;
+
+            // Asegurar que tenga http
+            if (!url.StartsWith("http"))
+            {
+                url = "https://" + url;
+            }
+
+            webView21.CoreWebView2.Navigate(url);
+
+            // Guardar en historial
+            Guardar(archivoHistorial, url);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Evento Load del formulario
+        private async void Form1_Load_1(object sender, EventArgs e)
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+
+            if (File.Exists(archivoHistorial))
+             {
+                FileStream stream = new FileStream(archivoHistorial, FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(stream);
+
+                 comboBox1.Items.Clear();
+
+                    while (reader.Peek() > -1)
+                    {
+                    string linea = reader.ReadLine();
+                    comboBox1.Items.Add(linea);
+                    }
+
+        reader.Close();
+    }
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
